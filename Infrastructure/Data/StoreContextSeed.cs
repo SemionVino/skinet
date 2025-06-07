dd5e69ae3a79418701418597562d3306319628d1
Semion <semion@jetweb.co.il>
2025-06-07 20:59:31 +0300

    dddd

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,19 @@
+﻿
+using Core.Entities;
+using Core.Interfaces;
+using System.Text.Json;
+
+namespace Infrastructure.Data;
+public class StoreContextSeed {
+    public static async Task SeedAsync (StoreContext context) {
+        if (!context.Products.Any()) {
+            var jsonString = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+            var productList = JsonSerializer.Deserialize<List<Product>>(jsonString);
+            if (productList == null)
+                return;
+            context.Products.AddRange(productList);
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
